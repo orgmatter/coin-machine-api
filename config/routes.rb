1e5c3da50371d801289eed1ee607445a8f4e1a9a
ogadimma osuji <ogadimma.osuji@mkobo.com.ng>
2020-03-25 18:07:34 +0100

    added namespaces to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    collection do
-      get :total_coins_value
-    end
-    collection do
-      get :user_transactions
-      get :transactions
-    end
-    resources :coins do
-      resources :transactions
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        collection do
+          get :total_coins_value
+        end
+        collection do
+          get :user_transactions
+          get :transactions
+        end
+        resources :coins do
+          resources :transactions
+        end
+      end
+
+      resources :admin
     end
   end
-
-  resources :admin
 
   # get '/users/:user_id/total_coins_value' => 'coins#total_coins_value', as: :users_total_coins_value
   # get '/users/:user_id/user_transactions' => 'transactions#user_transactions', as: :users_user_transaction
