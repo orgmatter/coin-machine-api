fd424ef2b6d216752095d393471c7ac69c0ef0bf
ogadimma osuji <ogadimma.osuji@mkobo.com.ng>
2020-03-26 09:29:13 +0100

    changes in route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v1 do
-      resources :users do
-        collection do
-          get :total_coins_value
-        end
-        collection do
-          get :user_transactions
-          get :transactions
-        end
-        resources :coins do
-          resources :transactions
-        end
-      end
-
-      resources :admin
+  resources :users do
+    collection do
+      get :total_coins_value
+    end
+    collection do
+      get :user_transactions
+      get :transactions
+    end
+    resources :coins do
+      resources :transactions
     end
   end
 
-  # get '/users/:user_id/total_coins_value' => 'coins#total_coins_value', as: :users_total_coins_value
-  # get '/users/:user_id/user_transactions' => 'transactions#user_transactions', as: :users_user_transaction
-  # get '/users/:user_id/transactions' => 'transactions#all_transactions', as: :users_transactions
+  resources :admin
 end
